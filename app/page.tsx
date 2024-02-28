8313fa4231d9dea0c87753b16583d94b22fc9ae3
Sachin Gupta <132441441+Sachng13@users.noreply.github.com>
2024-02-28 10:54:18 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <About />
         <Experience />
         {/* <Certificates/> */}
-{/*         <Tech/> */}
+        <Tech/>
         <Projects />
         <Footer />
       </div>
